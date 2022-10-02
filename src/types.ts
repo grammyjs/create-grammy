fad52f70e4d77779667079dfaf1ea5d078e1e5ec
dcdunkan <70066170+dcdunkan@users.noreply.github.com>
2022-10-02 21:10:26 +0530

    remove unwanted files

--- src/types.ts
+++
@@ -1,8 +0,0 @@
-export type PromtOptions = {
-  initial?: string,
-  choices?: Array<{ title: string, value: any }>
-  message: string,
-  validate?: (params: any) => boolean,
-  onCancel?: () => void,
-  type: 'text' | 'select'
-}
--- templates/node/base-runner/README.md
+++
@@ -1,3 +0,0 @@
-## Introduction
-
-This is base node example of grammY runner.
