7c381e857da02010d556e1fce3387431bdf21a4b
dcdunkan <70066170+dcdunkan@users.noreply.github.com>
2022-10-05 01:35:20 +0530

    make error message constant

--- internal/files/package_name.go
+++ internal/files/package_name.go
@@ -13,28 +13,30 @@ func UpdatePackageName(dir string) error {
 		return errors.New("failed to read project folder")
 	}
 
+	writeError := errors.New("failed to write package.json")
+
 	for _, file := range files {
 		if file.Name() == "package.json" {
 			path := filepath.Join(dir, "package.json")
 			bytes, err := os.ReadFile(path)
 			if err != nil {
-				return errors.New("failed to write package.json")
+				return writeError
 			}
 
 			var packageJson map[string]interface{}
 			err = json.Unmarshal(bytes, &packageJson)
 			if err != nil {
-				return errors.New("failed to write package.json")
+				return writeError
 			}
 
 			packageJson["name"] = dir
 			data, err := json.Marshal(packageJson)
 			if err != nil {
-				return errors.New("failed to write package.json")
+				return writeError
 			}
 			err = os.WriteFile(path, data, 0777)
 			if err != nil {
-				return errors.New("failed to write package.json")
+				return writeError
 			}
 		}
 	}
